a021250a734bc636276a1abe20a435a71c8dd705
John-Philipp Vogt <jpv-iuk@mailbox.org>
2026-06-14 23:00:37 +0200

    more work

--- .config/rofi/scripts/powermenu.sh
+++ .config/rofi/scripts/powermenu.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 
-options="  Lock\n  Logout\n  Suspend\n  Hibernate\n  Reboot\n  Shutdown"
+options="  Lock\n  Logout\n  Suspend\n  Hibernate\n  Reboot\n  Shutdown\n Performance\n Balanced\n Power Saver "
 
 selected=$(echo -e "$options" | rofi -dmenu -i -p "Power" -matching fuzzy)
 
@@ -13,5 +13,8 @@ case $selected in
     *Hibernate) systemctl hibernate ;;
     *Reboot)    systemctl reboot ;;
     *Shutdown)  systemctl poweroff ;;
+    *Performance) powerprofilesctl set performance ;;
+    *Balanced)    powerprofilesctl set balanced ;;
+    *Power\ Saver) powerprofilesctl set power-saver ;;
 esac
 
